82fc0cc0223390bee63220b96b3fcad631acec63
SmartDuck9000 <kulikovgeorgij000@gmail.com>
2021-01-30 19:14:26 +0300

    #1 add connection to db with gorm and first step configuring connection pool

--- services/user_service/db/db.go
+++ services/user_service/db/db.go
@@ -1,7 +1,13 @@
 package db
 
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"time"
+)
+
 type TravellyDb interface {
-	Open()
+	Open() error
 	Close()
 
 	GetUser(userId int) *User
@@ -15,14 +21,35 @@ type TravellyDb interface {
 }
 
 type TravellyPostgres struct {
+	url  string
+	conn *gorm.DB
 }
 
-func (db TravellyPostgres) Open() {
-
+func (db TravellyPostgres) Open() error {
+	var err error
+	db.conn, err = gorm.Open(postgres.Open(db.url), &gorm.Config{})
+	if err == nil {
+		err = db.configureConnectionPools()
+	}
+	return err
 }
 
-func (db TravellyPostgres) Close() {
+func (db TravellyPostgres) configureConnectionPools() error {
+	sqlDB, err := db.conn.DB()
+	if err != nil {
+		return err
+	}
 
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	sqlDB.SetMaxIdleConns(10)
+
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	sqlDB.SetMaxOpenConns(100)
+
+	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	return nil
 }
 
 func (db TravellyPostgres) GetUser(userId int) *User {
