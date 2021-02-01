6d30c816aeebe124f1241e6745f49c233b50206c
SmartDuck9000 <kulikovgeorgij000@gmail.com>
2021-02-01 04:53:53 +0300

    #1 add routing and prepare handlers

---
+++ services/user_service/server/api_handlers.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func (api UserServiceAPI) getUser(c *gin.Context) {
+
+}
+
+func (api UserServiceAPI) getTours(c *gin.Context) {
+
+}
+
+func (api UserServiceAPI) getCityTours(c *gin.Context) {
+
+}
+
+func (api UserServiceAPI) getCityTourEvents(c *gin.Context) {
+
+}
+
+func (api UserServiceAPI) getCityTourRestaurantBookings(c *gin.Context) {
+
+}
+
+func (api UserServiceAPI) getCityTourTickets(c *gin.Context) {
+
+}
+
+func (api UserServiceAPI) getCityTourHotel(c *gin.Context) {
+
+}
--- services/user_service/server/server.go
+++ services/user_service/server/server.go
@@ -18,12 +18,22 @@ type UserServiceAPI struct {
 }
 
 func CreateServer(conf config.UserServiceConfig) *UserServiceAPI {
-	return &UserServiceAPI{
+	var api = UserServiceAPI{
 		server: gin.Default(),
 		db:     db.CreateUserServiceDb(conf.DB),
 		host:   conf.Host,
 		port:   conf.Port,
 	}
+
+	api.server.GET("/api/users", api.getUser)
+	api.server.GET("/api/users/tours", api.getTours)
+	api.server.GET("/api/users/tours/city_tours", api.getCityTours)
+	api.server.GET("/api/users/tours/city_tours/events", api.getCityTourEvents)
+	api.server.GET("/api/users/tours/city_tours/restaurant_bookings", api.getCityTourRestaurantBookings)
+	api.server.GET("/api/users/tours/city_tours/tickets", api.getCityTourTickets)
+	api.server.GET("/api/users/tours/city_tours/hotels", api.getCityTourHotel)
+
+	return &api
 }
 
 func (api UserServiceAPI) Run() error {
