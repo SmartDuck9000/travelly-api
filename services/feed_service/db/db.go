04ebfb4a6497d0518dfa73b17cf92d9602dfba81
SmartDuck9000 <kulikovgeorgij000@gmail.com>
2021-02-04 22:51:26 +0300

    #4 add simple db creation

--- services/feed_service/db/db.go
+++ services/feed_service/db/db.go
@@ -1,1 +1,46 @@
 package db
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"time"
+)
+
+type FeedDb interface {
+	Open() error
+	configureConnectionPools() error
+
+	GetHotels(orderedBy string) []Hotel
+	GetEvents(orderedBy string) []Event
+	GetRestaurants(orderedBy string) []Restaurant
+}
+
+type FeedPostgres struct {
+	url             string
+	maxIdleConn     int
+	maxOpenConn     int
+	connMaxLifetime time.Duration
+	conn            *gorm.DB
+}
+
+func (db FeedPostgres) Open() error {
+	var err error
+	db.conn, err = gorm.Open(postgres.Open(db.url), &gorm.Config{})
+	if err == nil {
+		err = db.configureConnectionPools()
+	}
+	return err
+}
+
+func (db FeedPostgres) configureConnectionPools() error {
+	sqlDB, err := db.conn.DB()
+	if err != nil {
+		return err
+	}
+
+	sqlDB.SetMaxIdleConns(db.maxIdleConn)
+	sqlDB.SetMaxOpenConns(db.maxOpenConn)
+	sqlDB.SetConnMaxLifetime(db.connMaxLifetime)
+
+	return nil
+}
