3a5e13fa36bdb3193c9315fdd4ef3a60cf60cadb
SmartDuck9000 <kulikovgeorgij000@gmail.com>
2021-02-04 22:55:35 +0300

    #4 add creation db from config func

--- services/feed_service/db/db.go
+++ services/feed_service/db/db.go
@@ -1,12 +1,13 @@
 package db
 
 import (
+	"github.com/SmartDuck9000/travelly-api/services/feed_service/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"time"
 )
 
-type FeedDb interface {
+type FeedDB interface {
 	Open() error
 	configureConnectionPools() error
 
@@ -21,6 +22,16 @@ type FeedPostgres struct {
 	maxOpenConn     int
 	connMaxLifetime time.Duration
 	conn            *gorm.DB
+}
+
+func CreateFeedServiceDB(conf config.FeedDBConfig) *FeedPostgres {
+	return &FeedPostgres{
+		url:             conf.URL,
+		maxIdleConn:     conf.MaxIdleConn,     // maximum number of connections in the idle connection pool
+		maxOpenConn:     conf.MaxOpenConn,     // maximum number of open connections to the database
+		connMaxLifetime: conf.ConnMaxLifetime, // maximum amount of time a connection may be reused
+		conn:            nil,
+	}
 }
 
 func (db FeedPostgres) Open() error {
