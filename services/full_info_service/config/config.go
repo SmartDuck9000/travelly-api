ea23d01685d162ee3c8cf3921d7af36b404da95a
SmartDuck9000 <kulikovgeorgij000@gmail.com>
2021-02-05 21:33:54 +0300

    #5 add config reading, example for config file and init environment

---
+++ services/full_info_service/config/config.go
@@ -0,0 +1,63 @@
+package config
+
+import (
+	"os"
+	"strconv"
+	"time"
+)
+
+type FullInfoDBConfig struct {
+	URL             string
+	MaxIdleConn     int
+	MaxOpenConn     int
+	ConnMaxLifetime time.Duration
+}
+
+type FullInfoConfig struct {
+	DB   FullInfoDBConfig
+	Host string
+	Port string
+}
+
+// New returns a new Config struct
+func New() *FullInfoConfig {
+	return &FullInfoConfig{
+		DB: FullInfoDBConfig{
+			URL:             getEnv("DB_URL", ""),
+			MaxIdleConn:     getIntEnv("MAX_IDLE_CONN", 10),
+			MaxOpenConn:     getIntEnv("MAX_OPEN_CONN", 100),
+			ConnMaxLifetime: getHoursEnv("CONN_MAX_LIFETIME", 1),
+		},
+		Host: getEnv("HOST", ""),
+		Port: getEnv("PORT", ""),
+	}
+}
+
+// Simple helper function to read an environment or return a default value
+func getEnv(key string, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+
+	return defaultVal
+}
+
+// Simple helper function to read an environment variable into integer or return a default value
+func getIntEnv(name string, defaultVal int) int {
+	valueStr := getEnv(name, "")
+	if value, err := strconv.Atoi(valueStr); err == nil {
+		return value
+	}
+
+	return defaultVal
+}
+
+// Simple helper function to read an environment variable into time.Hour or return a default value
+func getHoursEnv(name string, defaultVal int) time.Duration {
+	valueStr := getEnv(name, "")
+	if value, err := strconv.Atoi(valueStr); err == nil {
+		return time.Hour * time.Duration(value)
+	}
+
+	return time.Hour * time.Duration(defaultVal)
+}
