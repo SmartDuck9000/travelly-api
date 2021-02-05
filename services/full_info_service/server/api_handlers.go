022557d235b1e2e2f0f0ec26aa8520cd4374e91a
SmartDuck9000 <kulikovgeorgij000@gmail.com>
2021-02-05 22:01:31 +0300

    #5 add api handlers for requests

--- services/full_info_service/server/api_handlers.go
+++ services/full_info_service/server/api_handlers.go
@@ -1,15 +1,37 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
 
 func (api FullInfoAPI) getHotel(c *gin.Context) {
-
+	id, err := strconv.Atoi(c.Query("id"))
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+	} else {
+		user := api.db.GetHotel(id)
+		c.JSON(http.StatusOK, *user)
+	}
 }
 
 func (api FullInfoAPI) getEvent(c *gin.Context) {
-
+	id, err := strconv.Atoi(c.Query("id"))
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+	} else {
+		user := api.db.GetEvent(id)
+		c.JSON(http.StatusOK, *user)
+	}
 }
 
 func (api FullInfoAPI) getRestaurant(c *gin.Context) {
-
+	id, err := strconv.Atoi(c.Query("id"))
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+	} else {
+		user := api.db.GetRestaurant(id)
+		c.JSON(http.StatusOK, *user)
+	}
 }
